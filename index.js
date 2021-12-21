79f512221e53ad2bfd95bca4c4aad50f85e01210
dieselleo <diesel.leonardo@gmail.com>
2021-12-21 15:12:10 +0000

    BS and JS for table from index.js to html added

--- index.js
+++ index.js
@@ -4,5 +4,30 @@ const   http = require('http'),
         express = require('express'),
         fs = require('fs'),
         xmlParse = require('xslt-processor').xmlParse,
-        xsltProcess = require('xslt-processor').xmlProcess,
-        xml2js = require('xml2js')
+        xsltProcess = require('xslt-processor').xsltProcess,
+        xml2js = require('xml2js');
+
+const   router = express(),
+        server = http.createServer(router);
+
+router.use(express.static(path.resolve(__dirname,'views')));
+
+router.get('/get/html', function(req, res){
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+
+    let xml = fs.readFileSync('DieselRecords.xml', 'utf8'),
+        xsl = fs.readFileSync('DieselRecords.xsl', 'utf8');
+    
+    let doc = xmlParse(xml),
+        stylesheet = xmlParse(xsl);
+
+    let result = xsltProcess(doc, stylesheet);
+
+    res.end(result.toString());
+
+})
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+    const addr = server.address();
+    console.log("Server listening at", addr.address + ":" + addr.port);
+})
